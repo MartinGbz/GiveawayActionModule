8e7bcbe50120f03ac35ada32bdc3a900a53992a0
MartinGbz <martin.grabarz@outlook.fr>
2024-01-14 16:42:37 +0100

    test: tests working

--- test/LensGiveawayOpenAction.t.sol
+++ test/LensGiveawayOpenAction.t.sol
@@ -8,12 +8,17 @@ import {LensGiveawayOpenAction} from "../src/LensGiveawayOpenAction.sol";
 import {LensGiveaway} from "../src/LensGiveaway.sol";
 import {Types} from 'lens/Types.sol';
 import {Types as GiveawayTypes} from 'lens-giveaway/Types.sol';
-// import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-// import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+
+abstract contract USDC {
+    function approve(address spender, uint256 value) public virtual returns (bool);
+    function balanceOf(address account) public view virtual returns (uint256);
+}
 
 contract LensGiveawayOpenActionTest is Test {
     LensGiveaway public lensGiveaway;
     LensGiveawayOpenAction public lensGiveawayOpenAction;
+
+    USDC internal usdc;
 
     // Mumbai
     // address public postAuthor = address(0xabEA470fb28074C7122585D289F658C5aC978B12); // profileId 17
@@ -22,7 +27,7 @@ contract LensGiveawayOpenActionTest is Test {
     // Polygon
     address public postAuthor = address(0x7241DDDec3A6aF367882eAF9651b87E1C7549Dff); // profileId 5
     address public me = address(0xa7073ca54734faBa5aFa5F1e01Cd31a03Ff7699F); // profileId 45190
-    address public usdc = address(0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174);
+    address public usdcAddress = address(0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174);
 
     // address public lensHubProxyMumbai = address(0x4fbffF20302F3326B20052ab9C217C44F6480900); // Mumbai
     address public lensHubProxyMumbai = address(0xDb46d1Dc155634FbC732f92E853b10B288AD5a1d); // Polygon
@@ -36,6 +41,7 @@ contract LensGiveawayOpenActionTest is Test {
     function setUp() public {
         lensGiveaway = new LensGiveaway();
         lensGiveawayOpenAction = new LensGiveawayOpenAction(lensHubProxyMumbai, address(lensGiveaway), me);
+        usdc = USDC(usdcAddress);
     }
 
     function testInit() public {
@@ -43,10 +49,10 @@ contract LensGiveawayOpenActionTest is Test {
 
         vm.startPrank(lensHubProxyMumbai); // OnlyHub
 
-        lensGiveawayOpenAction.initializePublicationAction(5, pubId, postAuthor,  abi.encode(usdc, 1));
+        lensGiveawayOpenAction.initializePublicationAction(5, pubId, postAuthor,  abi.encode(usdcAddress, 1));
         
         assertEq(lensGiveawayOpenAction.giveawayInfos(pubId).rewardAmount, 1);
-        assertEq(lensGiveawayOpenAction.giveawayInfos(pubId).rewardCurrency, usdc);
+        assertEq(lensGiveawayOpenAction.giveawayInfos(pubId).rewardCurrency, usdcAddress);
         assertEq(lensGiveawayOpenAction.giveawayInfos(pubId).usersRegistered, new address[](0));
         assertEq(lensGiveawayOpenAction.giveawayInfos(pubId).giveawayClosed, false);
 
@@ -55,7 +61,7 @@ contract LensGiveawayOpenActionTest is Test {
 
     function testProcess() public {
         vm.startPrank(lensHubProxyMumbai); // OnlyHub
-        lensGiveawayOpenAction.initializePublicationAction(authorProfileIdPolygon, pubId, postAuthor,  abi.encode(usdc, 1));
+        lensGiveawayOpenAction.initializePublicationAction(authorProfileIdPolygon, pubId, postAuthor,  abi.encode(usdcAddress, 1));
 
         assertEq(lensGiveawayOpenAction.giveawayInfos(pubId).rewardAmount, 1);
         assertEq(lensGiveawayOpenAction.giveawayInfos(pubId).rewardCurrency, 0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174);
@@ -67,7 +73,18 @@ contract LensGiveawayOpenActionTest is Test {
 
         assertEq(lensGiveawayOpenAction.giveawayInfos(pubId).usersRegistered.length, 1);
 
+        assertEq(usdc.balanceOf(me), 0);
+
+        vm.stopPrank();
+        vm.startPrank(postAuthor);
+        usdc.approve(address(lensGiveawayOpenAction), 1);
+        vm.stopPrank();
+        vm.startPrank(lensHubProxyMumbai); // OnlyHub
+
+        // postAuthor has USDC.e on Polygon on 14/01/2024
         Types.ProcessActionParams memory paramsDraw = Types.ProcessActionParams(authorProfileIdPolygon, pubId, authorProfileIdPolygon, postAuthor, postAuthor, new uint256[](0), new uint256[](0), new Types.PublicationType[](0), abi.encode(authorProfileIdPolygon));
         lensGiveawayOpenAction.processPublicationAction(paramsDraw);
+
+        assertEq(usdc.balanceOf(me), 1);
     }
 }
